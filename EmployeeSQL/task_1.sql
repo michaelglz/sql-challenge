799ebdbd614bc4ade11c3f2202641d12e73fd57c
michaelglz <53985218+michaelglz@users.noreply.github.com>
2020-02-15 10:08:41 -0600

    update

---
+++ EmployeeSQL/task_1.sql
@@ -0,0 +1,16 @@
+SELECT (emp_no) AS "Employee Number"
+FROM employees;
+
+SELECT (last_name) AS "Last Name"
+FROM employees;
+
+SELECT (first_name) AS "First Name"
+FROM employees;
+
+SELECT (gender) AS "Gender"
+FROM employees;
+
+SELECT (salary) AS "Salary"
+FROM salaries;
+
+
